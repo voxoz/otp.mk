5233be2b7c7adeecf4920cd7803410264558a918
Maxim Sokhatsky <maxim@synrc.com>
2014-02-13 06:47:03 +0200

    update otp.mk

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-RELEASE := skyline
+RELEASE := countach
 COOKIE  := node_runner
-APPS    := web amqp_client avz cowboy erlydtl gproc kai kvs lager mimetypes mqs n2o oauth rabbit_common ranch sync
+APPS    := web amqp_client avz cowboy erlydtl gproc kai kvs xmerl lager mimetypes mqs n2o n2o_bootstrap oauth rabbit_common ranch active feeds
 VER     := 1.0.0
 VM      := rels/web/files/vm.args
 SYS     := rels/web/files/sys.config
@@ -11,8 +11,9 @@ N2O          := deps/n2o/priv/static
 FILES        := apps/web/priv/static/n2o
 BOOTSTRAP    := apps/web/priv/static/bootstrap
 FONTAWESOME  := apps/web/priv/static/font-awesome
-MCE          := apps/web/priv/static/tinymce
-LESSJS       := apps/web/priv/static/less
+N2O_BT       := apps/web/priv/static/n2o_bt
+FD_UI        := apps/web/priv/static/feed_ui
+LESSJS       := apps/web/priv/static/less.js
 
 default: compile static-link
 static-link:
@@ -21,13 +22,15 @@ static-link:
 	rm -rf $(BOOTSTRAP)
 	rm -rf $(FONTAWESOME)
 	rm -rf $(LESSJS)
-	rm -rf $(MCE)
+	rm -rf $(N2O_BT)
+	rm -rf $(FD_UI)
 	ln -sf ../../n2o_scripts $(N2O)
 	mkdir -p $(shell dirname $(FILES))
-	ln -sf ../../../../deps/n2o/priv/static/tinymce $(MCE)
+	ln -sf ../../../../deps/n2o_bootstrap/priv/static $(N2O_BT)
+	ln -sf ../../../../deps/feeds/priv/static $(FD_UI)
 	ln -sf ../../../../deps/n2o/priv/static/n2o $(FILES)
-	ln -sf ../../../../deps/n2o/priv/static/bootstrap $(BOOTSTRAP)
-	ln -sf ../../../../deps/n2o/priv/static/font-awesome $(FONTAWESOME)
-	ln -sf ../../../../deps/n2o/priv/static/less $(LESSJS)
+	ln -sf ../../../../deps/bootstrap $(BOOTSTRAP)
+	ln -sf ../../../../deps/font-awesome $(FONTAWESOME)
+	ln -sf ../../../../deps/less.js $(LESSJS)
 
 include otp.mk
--- otp.mk
+++ otp.mk
@@ -5,13 +5,12 @@ comma := $(empty),$(empty)
 VSN   := $(shell expr substr `git rev-parse HEAD` 1 6)
 DATE  := $(shell git show -s --format="%ci" HEAD | sed -e 's/\+/Z/g' -e 's/-/./g' -e 's/ /-/g' -e 's/:/./g')
 ERL_LIBS := $(subst $(space),:,$(ROOTS))
-PLT_NAME := .dialyzer.plt
 relx  := "{release,{$(RELEASE),\"$(VER)\"},[$(subst $(space),$(comma),$(APPS))]}.\\n{include_erts,true}.\
 \\n{extended_start_script,true}.\\n{generate_start_script,true}.\\n{sys_config,\"$(SYS)\"}.\
 \\n{vm_args,\"$(VM)\"}.\\n{overlay,[{mkdir,\"log/sasl\"}]}."
 
 test: eunit ct
-compile: get-deps static-link
+compile: get-deps
 delete-deps get-deps compile clean update-deps:
 	rebar $@
 .applist:
@@ -19,7 +18,8 @@ delete-deps get-deps compile clean update-deps:
 $(RUN_DIR) $(LOG_DIR):
 	mkdir -p $(RUN_DIR) & mkdir -p $(LOG_DIR)
 console: .applist
-	ERL_LIBS=$(ERL_LIBS) erl $(ERL_ARGS) -eval '[ok = application:start(A) || A <- $(shell cat .applist)]'
+	ERL_LIBS=$(ERL_LIBS) erl $(ERL_ARGS) -eval \
+		'[ok = application:ensure_started(A, permanent) || A <- $(shell cat .applist)]'
 start: $(RUN_DIR) $(LOG_DIR) .applist
 	ERL_LIBS=$(ERL_LIBS) run_erl -daemon $(RUN_DIR)/ $(LOG_DIR)/ "exec $(MAKE) console"
 attach:
@@ -29,9 +29,9 @@ release:
 stop:
 	kill -9 `ps ax -o pid= -o command=|grep $(RELEASE)|grep $(COOKIE)|awk '{print $$1}'`
 $(PLT_NAME):
-	ERL_LIBS=$(ERL_LIBS) dialyzer --build_plt --output_plt $(PLT_NAME) --apps $(APPS) 
-dialyze: $(PLT_NAME)
-	dialyzer apps/*/ebin deps/*/ebin --plt $(PLT_NAME) --no_native -Werror_handling -Wunderspecs -Wrace_conditions
+	ERL_LIBS=deps dialyzer --build_plt --output_plt $(PLT_NAME) --apps $(APPS) || true
+dialyze: $(PLT_NAME) compile
+	dialyzer deps/*/ebin --plt $(PLT_NAME) --no_native -Werror_handling -Wunderspecs -Wrace_conditions
 tar:
 	tar zcvf $(RELEASE)-$(VSN)-$(DATE).tar.gz _rel/lib/*/ebin _rel/lib/*/priv _rel/bin _rel/releases
 eunit:
