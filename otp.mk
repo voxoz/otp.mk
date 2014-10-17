e5879046c1a9fd80882b2b9f8acdfe2bc3206ac3
Maxim Sokhatsky <maxim@synrc.com>
2014-10-17 22:45:47 +0300

    mad

--- otp.mk
+++ otp.mk
@@ -1,5 +1,11 @@
-VM       := rels/web/files/vm.args
-SYS      := rels/web/files/sys.config
+ifeq ($(OS),Windows_NT)
+    SEPARATOR=;
+else
+    SEPARATOR=:
+endif
+
+VM       := vm.args
+SYS      := sys.config
 PLT_NAME := ~/.n2o_dialyzer.plt
 ERL_ARGS := -args_file $(VM) -config $(SYS)
 RUN_DIR  ?= rels/web/devbox
@@ -10,25 +16,26 @@ space    := $(empty) $(empty)
 comma    := $(empty),$(empty)
 VSN      := $(shell git rev-parse HEAD | head -c 6)
 DATE     := $(shell date "+%Y%m%d-%H%M%S")
-ERL_LIBS := $(subst $(space),:,$(ROOTS))
+ERL_LIBS := $(subst $(space),$(SEPARATOR),$(ROOTS))
 relx     := "{release,{$(RELEASE),\"$(VER)\"},[$(RELEASE)]}.\\n{include_erts,true}.\
 \\n{extended_start_script,true}.\\n{generate_start_script,true}.\\n{sys_config,\"$(SYS)\"}.\
 \\n{vm_args,\"$(VM)\"}.\\n{overlay,[{mkdir,\"log/sasl\"}]}."
 
 test: eunit ct
-compile: get-deps
-delete-deps get-deps compile update-deps:
+delete-deps get-deps update-deps:
 	rebar $@
+compile:
+	rebar compile skip_deps=true
 clean:
 	rm -f .applist
 	rebar $@
 .applist:
 	$(eval APPS := $(subst deps/,,$(subst apps/,,$(shell find apps deps -maxdepth 1 -mindepth 1 -type d))))
-	./orderapps.erl $(APPS) > $@
+	mad plan
 $(RUN_DIR) $(LOG_DIR):
 	mkdir -p $(RUN_DIR) & mkdir -p $(LOG_DIR)
 console: .applist
-	ERL_LIBS=$(ERL_LIBS) erl +pc unicode $(ERL_ARGS) -eval '[application:start(A) || A <- $(shell cat .applist)]'
+	ERL_LIBS="$(ERL_LIBS)" erl +pc unicode $(ERL_ARGS) -eval '[application:start(A) || A <- $(shell cat .applist)]'
 start: $(RUN_DIR) $(LOG_DIR) .applist
 	RUN_ERL_LOG_GENERATIONS=1000 RUN_ERL_LOG_MAXSIZE=20000000 \
 	ERL_LIBS=$(ERL_LIBS) run_erl -daemon $(RUN_DIR)/ $(LOG_DIR)/ "exec $(MAKE) console"
